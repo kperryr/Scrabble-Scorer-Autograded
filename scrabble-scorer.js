a4d29daaf5c44029c9f232039d4624cb5ce4381f
Katherine <43319584+kperryr@users.noreply.github.com>
2024-07-12 01:14:08 -0400

    Assigment two finished

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -12,6 +12,7 @@ const oldPointStructure = {
   10: ['Q', 'Z']
 };
 
+//----------------------------------------------------------------------------------------------------------
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
@@ -29,89 +30,137 @@ function oldScrabbleScorer(word) {
 	return letterPoints;
  }
 
-// your job is to finish writing these functions and variables that we've named //
-// don't change the names or your program won't work as expected. //
+ 
 
+//----------------------------------------------------------------------------------------------------------
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
-   let userWord = input.question("Enter a word to score:");
-   while(!(/^[a-zA-Z]+$/.test(userWord))){
-      userWord = input.question("Error: Invalid characters. Please enter a word to score:");
+   console.log("Let's play some scrabble!\n");
+   let userWord = input.question("Enter a word to score: ");
+
+   while (!(/^[a-zA-Z]+$/.test(userWord))) { //checks if all are letters ie no spaces or numbers or symbols
+
+      userWord = input.question("Error: Invalid characters. Please enter a word to score: ");
+
    }
-   return userWord;
+
+   return userWord.toLowerCase();
 }
 
+
+
+//----------------------------------------------------------------------------------------------------------
 let newPointStructure = transform(oldPointStructure);
 
+
+
+//----------------------------------------------------------------------------------------------------------
 let simpleScorer = function(word){
    return word.length;
 };
 
+
+
+//----------------------------------------------------------------------------------------------------------
 let vowelBonusScorer = function(word){
    let vowels = ['A', 'E', 'I', 'O', 'U'];
    let score = 0;
+
    for (let i in word){
+
       if (vowels.includes(word[i].toUpperCase())){
          score += 3;
       }else{
          score += 1;
       }
+
    }
+
    return score;
 };
 
+
+
+//----------------------------------------------------------------------------------------------------------
 let scrabbleScorer = function(word){
    let score = 0;
+
    for (let i in word){
+
       score += newPointStructure[word[i]];
    }
    return score;
 };
 
+
+
+//----------------------------------------------------------------------------------------------------------
 const scoringAlgorithms = [
-   {"name":"Simple Score",
-    "description":"Each letter is worth 1 point.",
-    "scroerFunction":simpleScorer}, 
-
-   {"name":"Bonus Vowels",
-    "description":"Vowels are 3 pts, consonants are 1 pt.",
-    "scroerFunction":vowelBonusScorer},
-
-   {"name":"Scrabble",
-    "description":"The traditional scoring algorithm.",
-    "scroerFunction":scrabbleScorer}
+   { 
+      "name":"Simple Score",
+      "description":"Each letter is worth 1 point.",
+      "scorerFunction":simpleScorer
+   },
+   
+   {
+      "name":"Bonus Vowels",
+      "description":"Vowels are 3 pts, consonants are 1 pt.",
+      "scorerFunction":vowelBonusScorer
+   },
+   
+   {
+      "name":"Scrabble",
+      "description":"The traditional scoring algorithm.",
+      "scorerFunction":scrabbleScorer
+   }
 ];
 
+
+
+//----------------------------------------------------------------------------------------------------------
 function scorerPrompt(userWord) {
    let numArr = [0,1,2];
    console.log("Which scoring algorithm would you like to use?\n\n0 - Simple: One point per character\n1 - Vowel Bonus: Vowels are worth 3 points\n2 - Scrabble: Uses scrabble point system");
 
-   let algoNum = input.question("Enter 0, 1, or 2:");
+   let algoNum = input.question("Enter 0, 1, or 2: ");
    algoNum = Number(algoNum);
+
    while (!(numArr.includes(algoNum))){
-      algoNum = input.question("Error: Invalid Number. Enter 0, 1, or 2:");
+
+      algoNum = input.question("Error: Invalid Number. Enter 0, 1, or 2: ");
       algoNum = Number(algoNum);
+
    }
 
-   console.log(`Score for '${userWord}': ${scoringAlgorithms[algoNum].scroerFunction(userWord)}`);
+   console.log(`Score for '${userWord}': ${scoringAlgorithms[algoNum].scorerFunction(userWord)}`);
    
    return scoringAlgorithms[algoNum];
 }
 
+
+
+//----------------------------------------------------------------------------------------------------------
 function transform(obj) {
    let newObj = {};
    let arr = [];
+
    for (let key in obj){
+
       arr = obj[key];
       
       for(let i in arr){
+
          let lowerCaseKey = arr[i].toLowerCase();
          newObj[lowerCaseKey] = Number(key);
+
       }
    }
+
    return newObj;
 };
 
+
+
+//----------------------------------------------------------------------------------------------------------
 function runProgram() {
    let word_to_score = initialPrompt();
    scorerPrompt(word_to_score);
